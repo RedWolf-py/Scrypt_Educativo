3ee47d17e9396d1b3de9882831fbc7b084869e06
RedWolf-py <ale.a.s610@gmail.com>
2021-03-12 10:27:27 -0300

    First commit

---
+++ loguin.py
@@ -0,0 +1,35 @@
+from time import sleep
+
+for c in range(0, 3):
+	print('\033[32m=°=°=°=°=°=  Você foi Hackeado !!! °=°=°=°=°=°=\033[m')
+	sleep(1.0)
+	
+print(''' \033[31m
+.../(\..................
+..//\\...................
+.//..)_.-"""-._,-“"-....
+.\\.^,'_.\../._\.....)...
+..`././O\|..|/O\\\.../....
+....\.\_/|..|\_/ \_/....
+.....\....'.._.`/.......
+......(.:(_):.).........
+......`._.-._,'.........
+........................
+======= \033[33mRedWolf\033[31m ========\033[m''')
+
+print('\033[32mAcessando Dados, Com Direito Root !!! ')
+sleep(2.0)
+print('Acesso Root Negado !!! ')
+sleep(2.0)
+print('Acessando Remotamente,Servidor On-Line')
+sleep(3.0)
+print('Conexão Completada !!!\nCopiando Dados Para o Servidor\n\033[31m••••• Aguarde !!!•••••\033[m')
+sleep(2.0)
+for x in range(0, 101, 10):
+	print(f'\033[32mCopiando...{x}%')
+	sleep(1.0)
+	
+print('\033[31m°°°°°  ✓ Dados Copiado com Sucesso!!! °°°°°\033[m')
+sleep(3.0)
+print('\033[34m\nFoi Só uma brincadeira, pra te alerta a no\nabrir codigo sem antes abrir no seu terminal, ou origem desconhecida !!!! ')
+
